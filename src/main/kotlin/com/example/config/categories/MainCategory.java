d4f44d2c06a4425a1914f0e6b2f4d73c4ade0a80
brainage04 <thomasmcmah21@gmail.com>
2024-02-04 01:20:58 +1000

    added 1 config option so MoulConfig doesn't crash lol

--- src/main/kotlin/com/example/config/categories/MainCategory.java
+++ src/main/kotlin/com/example/config/categories/MainCategory.java
@@ -1,5 +1,12 @@
 package com.example.config.categories;
 
-public class MainCategory {
+import com.google.gson.annotations.Expose;
+import io.github.moulberry.moulconfig.annotations.ConfigEditorBoolean;
+import io.github.moulberry.moulconfig.annotations.ConfigOption;
 
+public class MainCategory {
+    @Expose
+    @ConfigOption(name = "Toggle (Boolean)", desc = "This is a boolean used to toggle things on and off.")
+    @ConfigEditorBoolean
+    public boolean testBoolean = false;
 }
