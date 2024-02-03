9ef7200c3ad70dab08f91f55adec06d162d00a49
brainage04 <thomasmcmah21@gmail.com>
2024-02-04 01:15:28 +1000

    ExampleMod.kt improvements + config keybind

--- src/main/kotlin/com/example/ExampleMod.kt
+++ src/main/kotlin/com/example/ExampleMod.kt
@@ -3,29 +3,54 @@ package com.example
 import com.example.commands.CommandManager
 import com.example.config.manager.ConfigManager
 import com.example.config.ExampleModConfig
+import com.example.keybinds.ConfigKeybind
+import net.minecraft.client.settings.KeyBinding
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.fml.client.registry.ClientRegistry
 import net.minecraftforge.fml.common.Loader
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import org.apache.logging.log4j.LogManager
 
 @Mod(modid = ExampleMod.MOD_ID, useMetadata = true)
 class ExampleMod {
+    private fun registerKeyBinds(vararg keybinds: KeyBinding?) {
+        for (keybind in keybinds) {
+            ClientRegistry.registerKeyBinding(keybind)
+        }
+    }
 
-    @Mod.EventHandler
-    fun init(event: FMLInitializationEvent) {
-        configManager = ConfigManager()
-        MinecraftForge.EVENT_BUS.register(configManager)
+    private fun registerEvents(vararg events: Any?) {
+        for (event in events) {
+            MinecraftForge.EVENT_BUS.register(event)
+        }
     }
 
     @Mod.EventHandler
     fun preInit(event: FMLPreInitializationEvent) {
         CommandManager()
+
+        registerKeyBinds(
+            configKeyBind
+        )
+    }
+
+    @Mod.EventHandler
+    fun init(event: FMLInitializationEvent) {
+        configManager = ConfigManager()
+        MinecraftForge.EVENT_BUS.register(configManager)
+
+        registerEvents(
+            configKeyBind
+        )
     }
 
     companion object {
         lateinit var configManager: ConfigManager
         const val MOD_ID = "examplemod"
+        const val MOD_NAME = "Example Mod"
+        val LOGGER = LogManager.getLogger(MOD_ID)
 
         @JvmStatic
         val version: String
@@ -33,5 +58,7 @@ class ExampleMod {
 
         val config: ExampleModConfig
             get() = configManager.config ?: error("config is null")
+
+        val configKeyBind = ConfigKeybind()
     }
 }
---
+++ src/main/kotlin/com/example/keybinds/ConfigKeyBind.kt
@@ -0,0 +1,16 @@
+package com.example.keybinds
+
+import com.example.ExampleMod
+import net.minecraft.client.settings.KeyBinding
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent
+import org.lwjgl.input.Keyboard
+
+class ConfigKeybind : KeyBinding("Open Config GUI", Keyboard.KEY_RETURN, ExampleMod.MOD_NAME) {
+    @SubscribeEvent
+    fun tick(event: TickEvent.ClientTickEvent) {
+        if (event.phase != TickEvent.Phase.END) return
+
+        if (this.isPressed) ExampleMod.configManager.openConfigGui()
+    }
+}
