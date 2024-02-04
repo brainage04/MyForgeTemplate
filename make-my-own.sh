0846c440de16868da42681e12975faf65b7ec178
brainage04 <thomasmcmah21@gmail.com>
2024-02-04 19:44:11 +1000

    added mcmod.info checks

--- make-my-own.sh
+++ make-my-own.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 print_help() {
-  echo "$0 <new package name> <project name> <modid>"
+  echo "$0 <new package name> <project name> <modid> <owner>"
 }
 
-if [ "$#" -ne "3" ]; then
+if [ "$#" -ne "4" ]; then
   print_help
   exit 0
 fi
@@ -14,16 +14,19 @@ echo "Updating $base"
 package_name="$1"
 project_name="$2"
 modid="$3"
+owner="$4"
 package_dir=$(echo "$package_name" | tr . /)
 echo "Setting package name to $package_name"
 echo "Setting project name to $project_name"
 echo "Setting package dir to $package_dir"
 echo "Setting mod id to $modid"
+echo "Setting owner to $owner"
 (
   set -x
   find "$base"/src/main -type f -exec sed -i s/examplemod/"$modid"/g\;s/ExampleMod/"$project_name"/g\;s/com.example/"$package_name"/g {} +
   sed -i s/com.example/"$package_name"/g\;s/examplemod/"$modid"/g "$base"/gradle.properties
   sed -i s/examplemod/"$project_name"/g "$base"/settings.gradle.kts
+  sed -i s/ExampleMod/"$project_name"/g\;s/brainage04/"$owner"/g\;s/\(.*\)"$owner"/\1brainage04/ "$base"/src/main/resources/mcmod.info
   mkdir -p "$base"/src/main/java/"$package_dir"
   mkdir -p "$base"/src/main/kotlin/"$package_dir"
   mv "$base"/src/main/java/com/example/* "$base"/src/main/java/"$package_dir"
